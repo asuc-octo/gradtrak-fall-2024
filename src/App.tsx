e121930031c72f53ccfa6d203bbb9f8cca637d9c
Khankamol Chor Kongrukgreatiyos <khankamolk@berkeley.edu>
2024-11-03 16:30:24 -0800

    stash changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import "./App.css";
-import Kanban from "./app/Kanban/Kanban"
-import SetUpPage from "./components/SetUpPage"
-import SemesterBlock from "./components/SemesterBlock"
+import Kanban from "./app/Kanban/Kanban";
+import Onboarding from "./app/Onboarding/Onboarding";
+import SetUpPage from "./components/SetUpPage";
+import SemesterBlock from "./components/SemesterBlock";
 
 function App() {
   const [selectedYear, setSelectedYear] = useState<number | null>(null);
   return (
     <>
       {/* <SetUpPage></SetUpPage> */}
-      <Kanban />
+      <Onboarding />
     </>
   );
 }
