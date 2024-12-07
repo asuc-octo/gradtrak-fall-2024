9d0df5f9405fe2e32393aa205c2b6e9063c9b957
Khankamol Chor Kongrukgreatiyos <khankamolk@berkeley.edu>
2024-12-07 12:25:10 -0800

    some styilization fixes

--- src/app/SemesterHome/SemesterHome.tsx
+++ src/app/SemesterHome/SemesterHome.tsx
@@ -11,7 +11,7 @@ function SemesterHome() {
   // Pretend this is the queried data
   // TODO: Request for minors to be added to backend
   const user = {
-    "name": "Khankamol Chor Kongrukgreatiyos",
+    "name": "Yuna Kim",
     "majors": ["Computer Science BA", "Data Science"],
     "minors": []
   }
@@ -28,32 +28,32 @@ function SemesterHome() {
     <>
       <Flex direction="row" height="100vh">
         {/* Side panel */}
-          <SidePanel 
-            name={user.name} 
-            majors={user.majors} 
-            minors={user.minors}
-            totalUnits={140}
-            transferUnits={40}
-            pnpTotal={15}
-          />
+        <SidePanel 
+          name={user.name} 
+          majors={user.majors} 
+          minors={user.minors}
+          totalUnits={140}
+          transferUnits={40}
+          pnpTotal={15}
+        />
 
         {/* Page body */}
-          <Flex direction="column" gap="32px">
-            <h3 className='semester-title'>Semesters</h3>
-            <Flex direction="row" gap="12px" className='semester-layout'>
-              <SemesterBlock selectedSemester={"Miscellaneous"} selectedYear={""}></SemesterBlock>
-                {years.map((year) => (
-                    <Flex key={year} className="year-element" direction="row" gap="12px">
-                      {/* Replace with the element you want to render */}
-                      <SemesterBlock selectedSemester={"Fall"} selectedYear={year}></SemesterBlock>
-                      <SemesterBlock selectedSemester={"Spring"} selectedYear={year}></SemesterBlock>
-                      {summerCheck &&
-                        <SemesterBlock selectedSemester={"Summer"} selectedYear={year}></SemesterBlock>
-                      }
-                    </Flex>
-                  ))}
-            </Flex>
+        <Flex direction="column">
+          <h3 className='semester-title'>Semesters</h3>
+          <Flex className='semester-layout'>
+            <SemesterBlock selectedSemester={"Miscellaneous"} selectedYear={""}></SemesterBlock>
+              {years.map((year) => (
+                  <Flex key={year} className="year-element" direction="row" gap="12px">
+                    {/* Replace with the element you want to render */}
+                    <SemesterBlock selectedSemester={"Fall"} selectedYear={year}></SemesterBlock>
+                    <SemesterBlock selectedSemester={"Spring"} selectedYear={year}></SemesterBlock>
+                    {summerCheck &&
+                      <SemesterBlock selectedSemester={"Summer"} selectedYear={year}></SemesterBlock>
+                    }
+                  </Flex>
+                ))}
           </Flex>
+        </Flex>
       </Flex>
     </>
   );
